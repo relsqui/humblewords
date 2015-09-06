fae6b67848e76a55c80faad92e8abea4e33ac95d
Finn Ellis <relsqui@chiliahedron.com>
2015-09-05 18:35:45 -0700

    Add instructions.

--- humble.sh
+++ humble.sh
@@ -2,6 +2,7 @@
 
 WORD_LIST="word_list.txt"
 VALID_GUESSES="valid_guesses.txt"
+WRAPWIDTH=80
 
 end_game() {
     echo
@@ -24,6 +25,11 @@ LOWER=`head -n 1 $WORD_LIST`
 UPPER=`tail -n 1 $WORD_LIST`
 GUESSES=1
 
+echo
+echo "I'm thinking of a word. Guess what it is, and I'll tell you whether your guess \
+comes before or after my word, alphabetically." | fmt -$WRAPWIDTH
+echo
+
 while read -p "Guess $GUESSES: " GUESS && [[ "$GUESS" != "$TARGET" ]]; do
     GUESS=`echo "$GUESS" | tr A-Z a-z`
     if grep -q "$GUESS" $VALID_GUESSES; then
@@ -42,6 +48,9 @@ while read -p "Guess $GUESSES: " GUESS && [[ "$GUESS" != "$TARGET" ]]; do
         fi
     else
         echo "Sorry, I don't know that word."
+        if echo "$GUESS" | grep -q '[^a-z]'; then
+            echo "(I don't know any words with non-letters or capitals.)"
+        fi
     fi
     echo "Range: $LOWER to $UPPER"
     echo
